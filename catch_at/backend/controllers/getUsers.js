d68f6ef2bd1f3bc8eaf03958ffc356a3ae676fd9
Roman <r.demyanov.anthill@gmail.com>
2022-03-25 18:04:34 +0300

    getMessagedUsers/done

--- catch_at/backend/controllers/getUsers.js
+++ catch_at/backend/controllers/getUsers.js
@@ -15,7 +15,7 @@ module.exports.getAllUsers = async function (req, res) {
 
 module.exports.getMessagedUsers = async function (req, res) {
     const allMessagedUsers = await sequelize.query(
-        `SELECT DISTINCT("recipientId"), "senderId","createdAt", LAST_VALUE("msg")
+        `SELECT DISTINCT("recipientId"), "senderId", LAST_VALUE("msg")
         OVER(PARTITION BY "recipientId", "senderId"
             ORDER BY "createdAt"
             RANGE BETWEEN 
@@ -31,34 +31,46 @@ module.exports.getMessagedUsers = async function (req, res) {
     let allUserMessages = [];
 
     for (i of allMessagedUsers[0]) {
+        const userWithTimestamp = await Message.findOne({
+            where: {
+                senderId: i.senderId,
+                recipientId: i.recipientId,
+                msg: i.message
+            }
+        })
+        const iTimeStamp = {
+            ...i, createdAt: userWithTimestamp.dataValues.createdAt
+        }
         const crossCompairing = allUserMessages.find((el) => {
-            if (el.recipientId === i.senderId && el.senderId === i.recipientId) return true;
+            if (el.recipientId === iTimeStamp.senderId && el.senderId === iTimeStamp.recipientId) return true;
             return false
         })
         console.log(crossCompairing, "crossCompaiting")
         if (crossCompairing) {
-            if (i.createdAt > crossCompairing.createdAt) {
+
+            if (iTimeStamp.createdAt > crossCompairing.createdAt) {
                 const indexOf = allUserMessages.indexOf(crossCompairing)
                 allUserMessages.splice(indexOf, 1)
-                
-                const senderName = await User.findOne({where: {id: i.senderId}})
-                const recipientName = await User.findOne({where: {id: i.recipientId}})
-                allUserMessages.push({...i, senderName: senderName.dataValues.username, recipientName: recipientName.dataValues.username})
+
+                const senderName = await User.findOne({ where: { id: iTimeStamp.senderId } })
+                const recipientName = await User.findOne({ where: { id: iTimeStamp.recipientId } })
+                allUserMessages.push({ ...iTimeStamp, senderName: senderName.dataValues.username, recipientName: recipientName.dataValues.username })
                 continue;
             }
             continue
         }
-
-        if (!allUserMessages.includes(i)){ 
-            const senderName = await User.findOne({where: {id: i.senderId}})
-            const recipientName = await User.findOne({where: {id: i.recipientId}})
-            allUserMessages.push({...i, senderName: senderName.dataValues.username, recipientName: recipientName.dataValues.username})
+        //Here is bug in user messages!
+        const senderName = await User.findOne({ where: { id: iTimeStamp.senderId } })
+        const recipientName = await User.findOne({ where: { id: iTimeStamp.recipientId } })
+        const parsedEl = { ...iTimeStamp, senderName: senderName.dataValues.username, recipientName: recipientName.dataValues.username }
+        if (!allUserMessages.includes(parsedEl)) {
+            allUserMessages.push({ ...iTimeStamp, senderName: senderName.dataValues.username, recipientName: recipientName.dataValues.username })
         }
     }
     console.log(allUserMessages, "allUserMessages")
-// Find Users by id
-    
-    const getNames = await User.findOne({where: {id: 1}})
+    // Find Users by id
+
+    const getNames = await User.findOne({ where: { id: 1 } })
     console.log(getNames.username, "getNames")
 
     return res.status(200).json(allUserMessages)
